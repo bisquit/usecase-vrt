c8f6c4e74548f912e3de4b53529f61fa394beacf
bisquit <qu1dpr09u0@gmail.com>
2022-04-17 02:15:48 +0900

    sc

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await page.screenshot({ path: 'screenshot.png', fullPage: true });
+  await page.goto('http://localhost:3000');
+  await page.screenshot({ path: 'screenshots/home.png', fullPage: true });
 });
