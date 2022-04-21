d2ce58da54af59b53b4858537e4fbd4602cb89a4
bisquit <qu1dpr09u0@gmail.com>
2022-04-21 17:04:28 +0900

    e2e

---
+++ playwright.config.ts
@@ -0,0 +1,16 @@
+// playwright.config.ts
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  webServer: {
+    command: 'yarn dev',
+    url: 'http://localhost:3000/',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    baseURL: 'http://localhost:3000/',
+  },
+};
+
+export default config;
